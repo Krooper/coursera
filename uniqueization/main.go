29e46c28c54ecb88651b8330a5a3a7fad08a4092
Krooper <98321262+Krooper@users.noreply.github.com>
2022-01-24 19:12:29 +0300

    added uniqization in & out files

--- uniqueization/main.go
+++ uniqueization/main.go
@@ -1,15 +1,45 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
-	in := bufio.NewScanner(os.Stdin)
-	for in.Scan() {
-		txt := in.Text()
-		fmt.Println(txt)
+	file, err := os.Open("data_map.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
+	defer file.Close()
+
+	data := make([]byte, 64)
+	alreadySeenData := make(map[byte]bool)
+	var str string
+	for {
+		_, err := file.Read(data)
+		for i := range data {
+			if _, found := alreadySeenData[data[i]]; found {
+				continue
+			} else {
+				alreadySeenData[data[i]] = true
+				str += string(data[i])
+			}
+		}
+		if err == io.EOF { // если конец файла
+			break // выходим из цикла
+		}
+	}
+	fileNew, errNew := os.Create("out_data.txt")
+
+	if errNew != nil {
+		fmt.Println("Unable to create file:", err)
+		os.Exit(1)
+	}
+	defer fileNew.Close()
+
+	fileNew.WriteString(str)
+
+	fmt.Println("Done.")
 }
